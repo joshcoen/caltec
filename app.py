030b188ab518e5fe6d19df48a90acec67df80101
joshcoen <josh.coen@valcolabs.com>
2016-07-06 02:01:10 +0000

    added tempaltes directory and bootstrap

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route("/")
-def main():
+@app.route('/')
+def home():
 	return "Hey there"
 
+@app.route('/welcome')
+def welcome():
+	return render_template('welcome.html')
+
 if __name__ == "__main__":
-	app.run(host="0.0.0.0")
+	app.run(host="0.0.0.0", debug=True)
