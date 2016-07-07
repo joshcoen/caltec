66b9af7f3ee3f226f25bf5fc70c18fa532f2936c
joshcoen <josh.coen@gmail.com>
2016-07-07 13:39:18 -0400

    updated app.py with proper database info

--- app.py
+++ app.py
@@ -28,14 +28,14 @@ def login():
     try:
         if request.method == 'POST':
             username_form  = request.form['username']
-            cur.execute("SELECT COUNT(1) FROM users WHERE name = {};"
+            cur.execute("SELECT COUNT(1) FROM users WHERE user_name = {};"
                         .format(username_form))
 
             if not cur.fetchone()[0]:
                 raise ServerError('Invalid username')
 
             password_form  = request.form['password']
-            cur.execute("SELECT pass FROM users WHERE name = {};"
+            cur.execute("SELECT user_pass FROM users WHERE user_name = {};"
                         .format(username_form))
 
             for row in cur.fetchall():
