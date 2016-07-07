57c5dead67e920185ac158aa3128b3df657dc49e
joshcoen <josh.coen@gmail.com>
2016-07-07 13:12:42 -0400

    added logic for mysql connection, updated login.html and created index.html

--- app.py
+++ app.py
@@ -1,25 +1,59 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, session, redirect, url_for, escape, request, render_template
+from hashlib import md5
+import MySQLdb
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="mysql", user="root", passwd="JAC2ufc1", db="login")
+    cur = db.cursor()
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+class ServerError(Exception):pass
+
 @app.route('/')
-def home():
-  return "Hey there"
+def index():
+    if 'username' in session:
+        return redirect(url_for('login'))
 
-@app.route('/welcome')
-def welcome():
-  return render_template('welcome.html')
+    username_session = escape(session['username']).capitalize()
+    return render_template('index.html', session_user_name=username_session)
 
-# route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if 'username' in session:
+        return redirect(url_for('index'))
+
     error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('home'))
+    try:
+        if request.method == 'POST':
+            username_form  = request.form['username']
+            cur.execute("SELECT COUNT(1) FROM users WHERE name = {};"
+                        .format(username_form))
+
+            if not cur.fetchone()[0]:
+                raise ServerError('Invalid username')
+
+            password_form  = request.form['password']
+            cur.execute("SELECT pass FROM users WHERE name = {};"
+                        .format(username_form))
+
+            for row in cur.fetchall():
+                if md5(password_form).hexdigest() == row[0]:
+                    session['username'] = request.form['username']
+                    return redirect(url_for('index'))
+
+            raise ServerError('Invalid password')
+    except ServerError as e:
+        error = str(e)
+
     return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
